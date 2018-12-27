62fd8bb2cc4c685d294964f9fd2ac5c4236934a6
Chaman Gurjar <chamangujjar162@gmail.com>
2018-12-27 12:30:20 +0530

    Save Data in CoreData

--- BolgReaderWithCoreData/ViewController.swift
+++ BolgReaderWithCoreData/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private lazy var context = appDelegate.persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        saveDataInCoreData()
     }
-
-
+    
+    private func saveDataInCoreData() {
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        newUser.setValue("Chaman", forKey: "username")
+        newUser.setValue("12345", forKey: "password")
+        newUser.setValue(25, forKey: "age")
+        
+        do {
+            try context.save()
+            print("Saved")
+        } catch  {
+            print("Error in saving data in core data")
+        }
+    }
+    
+    
 }
 
